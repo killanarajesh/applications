e615255cb2bdbf4263ad2421b9353f69ef8e8e32
killanarajesh <41316585+killanarajesh@users.noreply.github.com>
2018-07-19 14:25:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
-function getfile(file,callback){
-var kri= new XMLHttpRequest();
-kri.overrideMimeType("application/js");
-kri.open("GET",file,true)  ;
-kri.onreadystatechange=function() {
-  if(kri.readyState ===4 && kri.status == "200")
-  {
-    callback(kri.responseText);
-  }
-};
-kri.send(null);
+// function getfile(file,callback){
+// var kri= new XMLHttpRequest();
+// kri.overrideMimeType("application/js");
+// kri.open("GET",file,true)  ;
+// kri.onreadystatechange=function() {
+//   if(kri.readyState ===4 && kri.status == "200")
+//   {
+//     callback(kri.responseText);
+//   }
+// };
+// kri.send(null);
+// }
+// getfile("js/data.json",function(text){
+//   var data=JSON.parse(text);
+//   console.log(data);
+//   details(data.basic);
+// career(data.career);
+// education(data.education);
+// sample(data.skills);
+// })
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+  return fetch(file).then(response=>{
+    if(response.ok){
+      resolve(response.json());
+    }
+    else {
+      reject(new Error('error'));
+    }
+
+  })
+  })
 }
-getfile("js/data.json",function(text){
-  var data=JSON.parse(text);
-  console.log(data);
-  details(data.basic);
-career(data.career);
-})
+
+var newfile = loadjson("js/data.json");
+  newfile.then(data=>{
+    details(data.basic);
+  career(data.career);
+  education(data.education);
+  sample(data.skills);
+  ache(data.achievements);
+  })
 
 var child = document.querySelector(".childone");
 
@@ -47,8 +71,8 @@ var son = document.querySelector(".children");
 
 function career(det){
 
-  var ad=document.createElement("h4");
-  ad.textContent=det.ca;
+  var ad=document.createElement("h2");
+  ad.textContent=det.carrer;
   son.appendChild(ad);
 
   var line=document.createElement("hr");
@@ -57,4 +81,68 @@ function career(det){
   var ok=document.createElement("h3");
   ok.textContent=det.main;
   son.appendChild(ok);
+  var line=document.createElement("hr");
+  son.appendChild(line);
+  var a=document.createElement("h2");
+  a.textContent=det.edut;
+  son.appendChild(a);
+}
+function education(edu) {
+
+  var line=document.createElement("hr");
+  son.appendChild(line);
+  var a=document.createElement("h2");
+  a.textContent=edu.edut;
+  son.appendChild(a);
+
+for (var i = 0; i < edu.length; i++) {
+
+  var deg=document.createElement("h2");
+  deg.textContent=edu[i].degree;
+  son.appendChild(deg);
+  var ins=document.createElement("td");
+  ins.textContent=edu[i].institute;
+
+  var d=document.createElement("td");
+  d.textContent=edu[i].data;
+  son.appendChild(ins);
+  son.appendChild(d);
+}
+}
+function sample(kill) {
+  var line=document.createElement("hr");
+  son.appendChild(line);
+  var abc=document.createElement("h2");
+  abc.textContent="skills:";
+  son.appendChild(abc);
+  var skilldata=document.createElement("table");
+  skilldata.border="0";
+  son.appendChild(skilldata);
+  tabledata="";
+
+for(i=0;i < kill.length; i++)
+{
+  tabledata+="<tr><td>"+kill[i].title+":<td>"+kill[i].lang;
+}
+skilldata.innerHTML=tabledata;
+}
+function ache(kil) {
+  var line=document.createElement("hr");
+  son.appendChild(line);
+
+  var abcd=document.createElement("h2");
+  abcd.textContent="achievements:";
+  son.appendChild(abcd);
+
+  var achdata=document.createElement("table");
+  achdata.border="0";
+  son.appendChild(achdata);
+
+  tabledata="";
+
+for(i=0;i < kil.length; i++)
+{
+  tabledata+="<tr><td>"+kil[i].place+":<td>"+kil[i].got;
+}
+achdata.innerHTML=tabledata;
 }
